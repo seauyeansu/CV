b4e4b11df7b6360ecd83c441d0d6c3398a0efd05
giselle <seauyeansu@gmail.com>
2020-02-26 13:47:43 +0100

    updated three stars

--- app/index.js
+++ app/index.js
@@ -260,7 +260,7 @@ function App (props) {
                         color="#a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
-                        color="#e7eaf6" />
+                        color="#a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
                         color="#e7eaf6" />
@@ -283,7 +283,7 @@ function App (props) {
                         color="#a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
-                        color="#e7eaf6" />
+                        color="##a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
                         color="#e7eaf6" />
@@ -300,7 +300,7 @@ function App (props) {
                         color="#a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
-                        color="#e7eaf6" />
+                        color="##a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
                         color="#e7eaf6" />
@@ -320,7 +320,7 @@ function App (props) {
                         color="#a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
-                        color="#e7eaf6" />
+                        color="##a2a8d3" />
                       <FontAwesomeIcon
                         icon={ faStar }
                         color="#e7eaf6" />
